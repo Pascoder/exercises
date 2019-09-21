871a0511dbcd18dbcf81c42a9fe2c94b1af527a1
Pascal iMac <pascal.wyser@students.fhnw.ch>
2019-09-21 16:34:24 +0200

    Test

--- src/first/CalculatorMVC.java
+++ src/first/CalculatorMVC.java
@@ -1,10 +1,25 @@
 package first;
 
-public class CalculatorMVC {
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class CalculatorMVC extends Application{
+	private CalculatorView view;
+	private CalculatorController controller;
+	private CalculatorModel model;
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		launch(args);
 
+	}
+
+	@Override
+	public void start(Stage primaryStage)  {
+		model = new CalculatorModel();
+		view = newCalculatorView(primaryStage, model);
+		controller = new CalculatorController(model, view);
+		
+		
 	}
 
 }
