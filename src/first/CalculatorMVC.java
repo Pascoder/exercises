e693c3d3776ea392f7f2e4eaad3ae9b923d1da74
FrankMauchle <55541627+FrankMauchle@users.noreply.github.com>
2019-09-21 17:06:31 +0200

    1. View Quellcode hinzugefügt. 2. Calculator MVC --> nicht programmiertes ausgeblendet, damit Programm läuft

--- src/first/CalculatorMVC.java
+++ src/first/CalculatorMVC.java
@@ -1,27 +1,22 @@
 package first;
 
-import Exercises.CalculatorModel;
-import Exercises.CalculatorView;
 import javafx.application.Application;
 import javafx.stage.Stage;
 
 public class CalculatorMVC extends Application{
-	private CalculatorView view;
-	private CalculatorController controller;
-	private CalculatorModel model;
-
+private CalculatorModel model;
+private CalculatorView view;
+private CalculatorController controller;
 	public static void main(String[] args) {
-		launch(args);//kommentar
+		launch(args);
 
 	}
-
-	@Override
 	public void start(Stage PrimaryStage) {
 		model = new CalculatorModel();
 		view = new CalculatorView(PrimaryStage,model);
+		//Ausgeblendet damit Programm leuft
 		//controller = new CalculatorController(model,view);
 		view.start();
-		
 	}
 
 }
--- src/first/CalculatorView.java
+++ src/first/CalculatorView.java
@@ -1,5 +1,82 @@
 package first;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.GridPane;
+import javafx.scene.layout.VBox;
+import javafx.stage.Stage;
 
 public class CalculatorView {
+private TextField ausgabe;
+private Button eins,zwei,drei,vier,fuenf,sechs,sieben,acht,neun,nulll,plus,gleich,c;
+private Stage stage;
+private CalculatorModel model;
 
+
+public CalculatorView(Stage stage, CalculatorModel model) {
+	this.stage = stage;
+	this.model = model;
+	stage.setTitle("Calculator");
+	
+	
+	GridPane grid = new GridPane();
+	
+	//AusgabeFeld
+	this.ausgabe =  new TextField();
+	grid.add(ausgabe, 0, 0);
+	grid.setColumnSpan(ausgabe, 4); //2 Zellen für Ausgabe Feld benutzen, damit Format passt
+	
+	//1. Zeile
+	this.sieben = new Button ("  7  ");
+	grid.add(sieben, 0, 1);
+	this.acht = new Button ("  8  ");
+	grid.add(acht, 1,1);
+	this.neun = new Button ("  9  ");
+	grid.add(neun, 2, 1);
+	this.plus = new Button("  +  ");
+	grid.add(plus, 3, 1);
+	
+	//2.Zeile
+	this.vier = new Button("  4  ");
+	grid.add(vier, 0, 2);
+	this.fuenf = new Button ("  5  ");
+	grid.add(fuenf, 1, 2);
+	this.sechs = new Button ("  6  ");
+	grid.add(sechs, 2, 2);
+	this.c = new Button ("  C  ");
+	grid.add(c, 3, 2);
+	
+	//3.Zeile
+	this.drei = new Button ("  3  ");
+	grid.add(drei, 0, 3);
+	this.zwei = new Button("  2  ");
+	grid.add(zwei, 1, 3);
+	this.eins = new Button("  1  ");
+	grid.add(eins, 2, 3);
+	this.gleich = new Button("  =  \r\n" + 
+			"");
+	grid.add(gleich, 3, 3);
+	grid.setRowSpan(gleich, 2);//Zeilen anpassen für =
+	grid.setColumnSpan(gleich, 2);//Spalten anpassen für =
+	
+	//4.Zeile
+	this.nulll = new Button("	      0 	    ");
+	grid.add(nulll, 0, 4);
+	grid.setColumnSpan(nulll, 3);
+	
+	
+	//grid also die root wird der scene übergeben
+	Scene scene = new Scene(grid);
+	stage.setScene(scene);
+	
+
+	
+	
+}
+
+
+public void start() {
+	stage.show();
+	
+}
 }
