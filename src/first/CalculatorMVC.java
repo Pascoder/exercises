09eb03bcae924a2ca71c056e1f85037409e9381e
FrankMauchle <55541627+FrankMauchle@users.noreply.github.com>
2019-09-21 16:45:11 +0200

    PrimaryStage name korrigiert

--- src/first/CalculatorMVC.java
+++ src/first/CalculatorMVC.java
@@ -16,7 +16,7 @@ public class CalculatorMVC extends Application{
 	@Override
 	public void start(Stage primaryStage)  {
 		model = new CalculatorModel();
-		view = new CalculatorView(primaryStage, model);
+		view = new CalculatorView(PrimaryStage, model);
 		controller = new CalculatorController(model, view);
 		
 		
