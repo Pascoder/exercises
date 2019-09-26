bae1bc51d6e1e246a962beaf8f1b650b0705cfb0
Pascal iMac <pascal.wyser@students.fhnw.ch>
2019-09-26 23:29:21 +0200

    e

--- src/chess/ChessView.java
+++ src/chess/ChessView.java
@@ -1,22 +1,30 @@
 package chess;
 
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
 import javafx.scene.layout.GridPane;
 import javafx.stage.Stage;
 
 public class ChessView {
 	private ChessModel model;
 	private Stage stage;
+	private Button A1,A2,A3;
 	
 	
-protected ChessView(Stage stage, ChessModel model) {
+	
+public ChessView(Stage stage, ChessModel model) {
 	this.model = model;
 	this.stage = stage;
 	stage.setTitle("Chess Game");
 	
 	GridPane root = new GridPane();
 	
-	Scene scene = new Scene (root,1000,500);
+	A1=new Button("black");
+	
+	root.add(A1, 1, 1);
+	
+	Scene scene = new Scene (root,600,600);
+	stage.setScene(scene);
 	
 	
 	
