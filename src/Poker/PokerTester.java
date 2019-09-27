fb400d6f6869194b6a18752333f025963ed67a85
pascalwyser <pascalwyser@pascals-air.edu.ds.fhnw.ch>
2019-09-27 17:24:18 +0200

    Created classes CardDeck, PokerCard, PokerTester

---
+++ src/Poker/PokerTester.java
@@ -0,0 +1,15 @@
+package Poker;
+
+public class PokerTester {
+
+	public static void main(String[] args) {
+		
+			System.out.println();
+			CardDeck cd = new CardDeck();
+//			System.out.println(cd);
+
+			
+
+	}
+
+}
