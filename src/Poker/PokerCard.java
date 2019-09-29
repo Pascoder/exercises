8b9438c9ae42dceb6de40792a94c6ea32585ea8b
pascalwyser <pascalwyser@pascals-air.edu.ds.fhnw.ch>
2019-09-29 11:59:33 +0200

    CardDeck override toString Method

--- src/Poker/PokerCard.java
+++ src/Poker/PokerCard.java
@@ -23,6 +23,22 @@ public class PokerCard {
 		return "PokerCard [cardRank=" + cardRank + ", suit=" + suit + "]";
 	}
 
+	public int getCardRank() {
+		return cardRank;
+	}
+
+	public String getSuit() {
+		return suit;
+	}
+
+	public void setCardRank(int cardRank) {
+		this.cardRank = cardRank;
+	}
+
+	public void setSuit(String suit) {
+		this.suit = suit;
+	}
+
 	
 }
 	
