99857725e5aad5fd1d840bb2e7b15e5d2297bb32
Pascoder <55625920+Pascoder@users.noreply.github.com>
2019-09-30 17:04:59 +0200

    Update PokerCard.java

--- src/Poker/PokerCard.java
+++ src/Poker/PokerCard.java
@@ -11,22 +11,22 @@ public class PokerCard {
 	public PokerCard(CardRank cardRank, CardSuit cardSuit) {
 		
 		this.cardRank = cardRank;
-		this.cardSuit = cardSuit;
-		
-		
-		
+		this.cardSuit = cardSuit;	
 	}
 
 	public enum CardSuit {
-	
 	HEARTS, 
 	SPADES, 
 	CLUBS, 
-	DIAMONDS;
-
+	DIAMONDS
 	}
 	
-
+	
+	public enum CardRank {
+	TWO, THREE, FOUR, FIVE, SIX,
+	SEVEN, EIGHT, NINE, TEN,
+	JACK, QUEEN, KING, ACE
+	}
 	
 }
 	
