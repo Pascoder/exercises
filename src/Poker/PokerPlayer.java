15dbf6788be6d2c17c91b4a1a7caffa8a2496cae
Pascal iMac <pascal.wyser@students.fhnw.ch>
2019-10-03 19:49:27 +0200

    Boolean flush

--- src/Poker/PokerPlayer.java
+++ src/Poker/PokerPlayer.java
@@ -35,6 +35,18 @@ public HandType evaluateHand(PokerCard[] hand) {
 	//Method which evaluates the actual hand and returns a HandType
 	
 	
+	
+	boolean flush = true; //assume there is a flush
+	
+	for (int x=0; x<6; x++)
+	      {
+	            if ( hand[x].cardSuit.ordinal()  != hand[x+1].cardSuit.ordinal() )
+
+	                flush = false;
+	        }
+
+	
+	
 	return null;
 }
 	
