9ff1f944176a3f1b7c0e6a657f67f0a7bae6aa95
FrankMauchle <55541627+FrankMauchle@users.noreply.github.com>
2019-10-06 14:40:04 +0200

    Complete Poker Controller

--- src/Poker/PokerController.java
+++ src/Poker/PokerController.java
@@ -15,6 +15,8 @@ private PokerPlayer player;
 private int playernumber = 0;
 
 
+
+
 public PokerController(PokerModel model, PokerView view) {
 
 	this.model = model;
@@ -27,17 +29,24 @@ public PokerController(PokerModel model, PokerView view) {
 	deck.printDeck(deck); //Test ob deck erzeugt wird
 	
 	//Player
-	PokerPlayer player = new PokerPlayer(this.playernumber);
+	this.player = new PokerPlayer(this.playernumber);
 	this.playernumber++; //muss am Anfang des Spieles gefragt werden wie viele Spieler
 	
-	//PokerPlayer wird ganzes Deck uebergeben und daraus Hands generiert
+	//PokerPlayer wird ganzes Deck übergeben und daraus Hands generiert
 	player.generateHand(deck);
+	
+	model.getHandType(player);
+	
+	
+	
+	//!!nachher muss hier in PokerPlayer klasse handtype zuweisen
 	
 	//Shuffle method
 
 		view.shuffle.setOnAction((event)-> {
 
-		
+		deck.shuffleDeck(deck);
+		deck.printDeck(deck);
 
 		
 
