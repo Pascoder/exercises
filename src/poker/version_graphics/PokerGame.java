79e7c91e61d6cfd19c814a2f0708d4e4c9697b60
FrankMauchle <55541627+FrankMauchle@users.noreply.github.com>
2019-10-14 17:52:18 +0200

    einige Lösungen für das EnterMenu probiert, jedoch noch nichts brauchbares gefunden

--- src/poker/version_graphics/PokerGame.java
+++ src/poker/version_graphics/PokerGame.java
@@ -12,11 +12,12 @@ import poker.version_graphics.view.PokerGameView;
 public class PokerGame extends Application {
 	//two players are given, we can change here the number of players
 	
-	public static int NUM_PLAYERS = 2;
-	EnterMenu entermenu = new EnterMenu();
+	public static int NUM_PLAYERS = 1;
+	EnterMenu menu;
 	PokerGameModel model;
 	PokerGameView view;
 	PokerGameController controller;
+
 	
     public static void main(String[] args) {
         launch();
@@ -25,10 +26,35 @@ public class PokerGame extends Application {
     @Override
     public void start(Stage primaryStage) throws Exception {
     	// Create and initialize the MVC components
+
+    		
+    		startEnterMenu();
+    		startGUI(primaryStage);
+    				
+    		
+    	}
+
+	private void startEnterMenu() {
+		menu = new EnterMenu();
+		
+		
+		
+	}
+
+	private void startGUI(Stage primaryStage) throws Exception{
+		
+			model = new PokerGameModel();
+	       	view = new PokerGameView(primaryStage, model);
+	       	controller = new PokerGameController(model, view);
+	       	
+		
+		
+	}
     	
     	
-    	model = new PokerGameModel();
-    	view = new PokerGameView(primaryStage, model);
-    	controller = new PokerGameController(model, view);
-    }
+    	 
+    	
+    	
+    	
+    
 }
--- src/poker/version_graphics/view/EnterMenu.java
+++ src/poker/version_graphics/view/EnterMenu.java
@@ -2,25 +2,28 @@ package poker.version_graphics.view;
 
 import java.util.ArrayList;
 
+import javafx.event.Event;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
 import javafx.scene.layout.GridPane;
+import javafx.stage.Modality;
 import javafx.stage.Stage;
 import poker.version_graphics.PokerGame;
 
 public class EnterMenu{
 	
-	public static int playercounter = 0;
-	Button enter = new Button("Start Game");
+	private int counter = 0;
 	Button addplayer = new Button("Add Player");
+	Button enter = new Button("Start Game");
 	TextField t1 = new TextField();
-	private static Label name = new Label("Name of Player "+playercounter+": ");
+	private static Label name = new Label("Name of Player "+PokerGame.NUM_PLAYERS+": ");
 	static Stage startmenuStage = new Stage();
 	private int players = 0;
 	private ArrayList<String> playersname;
 	public boolean gamestarten = false;
+	
 	
 	
 public EnterMenu() {
@@ -28,10 +31,7 @@ public EnterMenu() {
 	 //kann nachher mit if schleife vor haupt scene gestellt werden, wenn Start Game gedrückt dann soll erst spiel angezeigt werden
    
 	GridPane entergrid = new GridPane();
-	
-	
-	
-	
+
 	
 	entergrid.add(name, 0, 0);
 	entergrid.add(t1, 1, 0);
@@ -40,36 +40,46 @@ public EnterMenu() {
 	entergrid.add(enter, 2, 1);
 	
 	Scene startmenuScene = new Scene(entergrid);
-	startmenuScene.getStylesheets().add(
-            getClass().getResource("poker.css").toExternalForm()); //CSS File muss noch ersetzt werden 
+	/*startmenuScene.getStylesheets().add(
+            getClass().getResource("poker.css").toExternalForm()); //CSS File muss noch ersetzt werden */
 	startmenuStage.setScene(startmenuScene);
+	
 	startmenuStage.show();
 	
-	enter.setOnAction(e-> StartGame());
-	addplayer.setOnAction(e-> addPlayer());
+	
+	this.addplayer.setOnAction(this::addPlayer);
+	this.enter.setOnAction(this::StartGame);
 }
 
 
 
-private void addPlayer() {
-	if(PokerGame.NUM_PLAYERS <=5) {
-		PokerGame.NUM_PLAYERS++;
-		name.setText("Name of Player "+PokerGame.NUM_PLAYERS+": ");
+private void addPlayer(Event e) {
+	if(counter <=5) {
+		counter ++;
+		name.setText("Name of Player "+counter+": ");
+		String nameumwandeln = t1.getText();
+		System.out.println(playersname);
+		//this.playersname.add(nameumwandeln);
 		
-	
-		this.playersname.add(t1.getText());
 	}
 	
+
 	
 }
 
 
 
 
-private void StartGame() {
-	// TODO Auto-generated method stub
-	gamestarten = true;
+public void StartGame(Event a) {
+	
+	System.out.println(this.counter);
+	PokerGame.NUM_PLAYERS = this.counter;
+	System.out.println(PokerGame.NUM_PLAYERS);
+	
 	startmenuStage.close();
+	
+	gamestarten =true;
+	
 
 	
 	
