6be507f4b1ddca6b84f4282c259b3871af3c6f56
pascalwyser <pascalwyser@192.168.0.10>
2019-10-15 16:13:14 +0200

    evaluateWinner() weitergemacht

--- src/poker/version_graphics/model/PokerGameModel.java
+++ src/poker/version_graphics/model/PokerGameModel.java
@@ -11,6 +11,7 @@ public class PokerGameModel {
 	private DeckOfCards deck;
 	private String winnerName;
 	
+	
 	public PokerGameModel() {
 		for (int i = 0; i < PokerGame.NUM_PLAYERS; i++) {
 			players.add(new Player("Player " + i));
@@ -28,14 +29,35 @@ public class PokerGameModel {
 	}
 	
 	public String evaluateWinner() {
-//	Methode soll die ArrayList<Player> sortieren und dann den ersten als Gewinner ausgeben--> ToDO!!!
-		String winnerName;
+/**	Methode klont die ArrayList<Player> und sortiert diese anhand des HandType der Spieler.
+ * Wenn der HandType der letzten beiden in der Liste gleich ist, wird ermittelt wer die höhere Hand hat
+ */
+		
 		ArrayList <Player> clone = (ArrayList<Player>) players.clone();
+		
 		Collections.sort(clone);
-		winnerName = clone.get(clone.size()-1).getPlayerName();
+		
+		HandType firstHand = clone.get(clone.size()-1).getHandType();
+		HandType secondHand = clone.get(clone.size()-2).getHandType();
+		
+		if(firstHand.equals(secondHand)) {
+			if(firstHand.ordinal()==0) {
+				int j = clone.get(0).getCards().get(Player.HAND_SIZE-1).compareTo(clone.get(1).getCards().get(Player.HAND_SIZE-1));
+				if(j==1) {
+					winnerName=clone.get(0).getPlayerName();
+				} else
+					if(j==-1) {
+						winnerName=clone.get(1).getPlayerName();
+					} 
+			}
+			
+			
+		}else winnerName = clone.get(clone.size()-1).getPlayerName();
+		
 		return winnerName;
 		
-	
-		
+
 	}
+	
+	
 }
